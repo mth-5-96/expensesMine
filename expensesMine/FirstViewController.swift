7b071ed13f75f9c5233e5324130d47b5d2371cf5
Martin Hochstraßer <hochstrasser@eyeled.de>
2017-09-15 09:58:41 +0200

    Initial Commit

---
+++ expensesMine/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  expensesMine
+//
+//  Created by Martin Hochstraßer on 15.09.17.
+//  Copyright © 2017 Martin Hochstraßer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
