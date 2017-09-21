9248780c22c0992ac230487021c2ecd422be191c
Martin Hochstraßer <hochstrasser@eyeled.de>
2017-09-21 13:46:34 +0200

    added Models.swift

---
+++ expensesMine/Models.swift
@@ -0,0 +1,29 @@
+//
+//  Models.swift
+//  expensesMine
+//
+//  Created by Martin Hochstraßer on 21.09.17.
+//  Copyright © 2017 Martin Hochstraßer. All rights reserved.
+//
+
+import RealmSwift
+
+class Category : Object {
+    
+    dynamic var name = ""
+    
+    dynamic var lastAccessed = Date()
+    
+    dynamic var accountName = ""
+    
+}
+
+class AccountingEntry : Object {
+    
+    dynamic var amount : Double = 0.0
+    
+    dynamic var note = ""
+    
+    dynamic var category : Category?
+    
+}
