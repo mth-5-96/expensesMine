818d552113bdbd171002854cb8439a7faef572b1
Martin Hochstraßer <hochstrasser@eyeled.de>
2017-10-16 16:34:14 +0200

    showing real data

--- expensesMine/HistoryViewController.swift
+++ expensesMine/HistoryViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class HistoryViewController : UITableViewController {
     
@@ -22,12 +23,29 @@ class HistoryViewController : UITableViewController {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAdd))
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        items.removeAll()
+        
+        do {
+            let realm = try Realm()
+            let entries = realm.objects(AccountingEntry.self).filter("date BETWEEN {%@, %@}", startDate, endDate)
+            items.append([])
+            items[0].append(contentsOf: entries)
+        } catch {
+            print(error)
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return items[0].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! HistoryEntryCell
+        cell.amountLabel.text = "-100.00 €"
+        cell.categoryLabel.text = items[0][indexPath.row].category?.name
+        cell.noteLabel.text = items[0][indexPath.row].note
+        return cell
     }
     
     func handleAdd(_ sender: AnyObject?) {
