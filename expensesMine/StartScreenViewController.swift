20acc60586bdbb5e3d0f99d4a4b7b2652ddd5fff
Martin Hochstraßer <martin.hochstrasser@gmail.com>
2019-08-20 20:13:57 +0200

    added an gross amount label

---
+++ expensesMine/StartScreenViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StartScreenViewController.swift
+//  expensesMine
+//
+//  Created by Martin Hochstraßer on 20.08.19.
+//  Copyright © 2019 Martin Hochstraßer. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class StartScreenViewController : UIViewController {
+    
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        do {
+            let realm = try Realm()
+            let entries = realm.objects(AccountingEntry.self)
+            
+            let sum = Array(entries).reduce(0.0) { (sum, entry) -> Double in
+                sum + entry.amount
+            }
+            
+            amountLabel.text = "\(sum) €"
+        } catch {
+            print(error)
+        }
+    }
+}
